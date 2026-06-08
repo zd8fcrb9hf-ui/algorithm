64d6742aa9b57acfda8b32919c37fc34ed3df433
zd8fcrb9hf-ui <zd8fcrb9hf@privaterelay.appleid.com>
2026-06-08 11:57:12 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 3.71 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+  public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings);
+        Arrays.sort(strings, Comparator.comparing(string -> string.charAt(n)));
+
+        return strings;
+    }
+}
